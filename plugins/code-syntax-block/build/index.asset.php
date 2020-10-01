625e49faf6a8c24397ce7276520847a80e2a5c19
Bryan Davis <bd808@wikimedia.org>
2020-10-01 15:25:37 -0600

    plugins: Add code-syntax-block v1.3.5
    Add code-syntax-block v.1.3.5 downloaded from
    <https://github.com/mkaz/code-syntax-block/releases/download/v1.3.5/code-syntax-block-1.3.5.zip>
    
    Bug: T264223

---
+++ plugins/code-syntax-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'fbdeedddcc72a39f321264c7f8f0ccd4');
