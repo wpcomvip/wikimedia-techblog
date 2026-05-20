11bdd524449b215bd09861467fef3f2a5491e7e8
Chris Koerner <ckoerner@wikimedia.org>
2026-05-20 11:58:08 -0500

    Update vip-config.php
    add redirect to diff domain
    
    Signed-off-by: Chris Koerner <ckoerner@wikimedia.org>

--- vip-config/vip-config.php
+++ vip-config/vip-config.php
@@ -15,3 +15,26 @@
  *
  * - The WordPress.com VIP Team
  **/
+if ( isset( $_SERVER['HTTP_HOST'] ) && isset( $_SERVER['REQUEST_URI'] ) ) {
+	$http_host   = $_SERVER['HTTP_HOST']; // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+	$request_uri = $_SERVER['REQUEST_URI']; // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+
+	$redirect_to_domain = 'diff.wikimedia.org';
+	$redirect_domains   = array(
+		'techblog.wikimedia.org',
+	);
+
+	/**
+	 * Safety checks for redirection:
+	 * 1. Don't redirect for '/cache-healthcheck?' or monitoring will break
+	 * 2. Don't redirect in WP CLI context
+	 */
+	if (
+			'/cache-healthcheck?' !== $request_uri && // Do not redirect VIP's monitoring.
+			! ( defined( 'WP_CLI' ) && WP_CLI ) && // Do not redirect WP-CLI commands.
+			$redirect_to_domain !== $http_host && in_array( $http_host, $redirect_domains, true )
+		) {
+		header( 'Location: https://' . $redirect_to_domain . $request_uri, true, 301 );
+		exit;
+	}
+} t
